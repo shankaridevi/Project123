20d8462405d73ae0f64e9bb07b7536abfaf23ffe
shankaridevi <shankari90devi@gmail.com>
2022-09-30 21:29:36 -0700

    i pushed my project

---
+++ src/test/java/org/pro/TestP.java
@@ -0,0 +1,24 @@
+package org.pro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestP {
+	
+	public static void main(String[] args) {
+		int a[]= {1,2,3,5,6};
+		int e=a.length+1;
+		int sum=0;
+		int n=0,s=0;
+		for(int i=0;i<a.length;i++) {
+			sum=sum+a[i];
+			
+		}
+		n=(e*(e+1))/2;
+		
+		s=n-sum;
+		System.out.println(s);
+		
+	}
+	
+
+}
